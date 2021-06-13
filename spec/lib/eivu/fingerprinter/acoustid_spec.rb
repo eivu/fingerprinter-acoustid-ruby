fd2bd5fa510d6f4439d6eeaffca81f89119ec017
Robert Jenkins <bobert@gmail.com>
2021-06-13 17:09:53 -0400

    progress refactoring specs

--- spec/lib/eivu/fingerprinter/acoustid_spec.rb
+++ spec/lib/eivu/fingerprinter/acoustid_spec.rb
@@ -31,22 +31,229 @@ describe Eivu::Fingerprinter::Acoustid do
       end
     end
 
-    context 'Watch the throne - Made in America' do
-      context 'sending full file' do
-        let(:file) { '01-11-Made In America [feat Frank Ocean] [Explicit].mp3' }
-        let(:fingerprint) { 'spec/fixtures/fingerprints/made_in_america.txt' }
+    context 'Album: Watch the throne' do
+      let(:folder) { 'spec/fixtures/fingerprints/watch_the_throne/watch_the_throne/' }
 
-        context 'success' do
-          context 'when passing in audio file' do
-            it 'works' do
-              expect(identify['results'].count).to be(2)
-              expect(identify['status']).to eq('ok')
-              expect(identify.dig('results', 0, 'id')).to eq('aceb68bd-1447-4a6d-94b3-438772efbbdd')
-              expect(identify.dig('results', 1, 'id')).to eq('99c99bc6-77d4-4163-a9b1-cb4f67aee738')
-            end
+      context '01 - No Church in the wild ' do
+        let(:file) { 'No Church In The Wild.mp3' }
+        let(:fingerprint) { "#{folder}01-01-No Church In The Wild.txt" }
+
+        it 'returns the correct result' do
+          aggregate_failures do
+            expect(identify['results'].count).to be(1)
+            expect(identify['status']).to eq('ok')
+            expect(identify.dig('results', 0, 'id')).to eq('20daef69-936d-4a94-9ce3-5886b839e5a0')
           end
         end
       end
+
+      context '02 - Lift Off' do
+        let(:file) { '01-02- Lift Off.mp3' }
+        let(:fingerprint) { "#{folder}01-02- Lift Off.txt" }
+
+        it 'returns the correct result' do
+          aggregate_failures do
+            expect(identify['results'].count).to be(1)
+            expect(identify['status']).to eq('ok')
+            expect(identify.dig('results', 0, 'id')).to eq('34001c38-a098-44a6-8166-39e03475d18e')
+          end
+        end
+      end
+
+      context '03 - 01-03- Ni**as In Paris' do
+        let(:file) { '01-03- Ni**as In Paris.mp3' }
+        let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+        it 'returns the correct result' do
+          aggregate_failures do
+            expect(identify['results'].count).to be(2)
+            expect(identify['status']).to eq('ok')
+            expect(identify.dig('results', 0, 'id')).to eq('b4c008df-ce21-4950-b7ef-5e7f032bfb32')
+            expect(identify.dig('results', 1, 'id')).to eq('1844c1f9-2711-41c5-9064-bb9b1e588bb9')
+          end
+        end
+      end
+
+      # context '04 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
+
+      # context '05 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
+
+      # context '06 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
+
+      # context '07 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
+
+      # context '08 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
+
+      # context '09 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
+
+      # context '10 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
+
+      # context '11 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
+
+      # context '12 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
+
+      # context '13 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
+
+      # context '14 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
+
+      # context '15 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #   end
+      # end
+
+      # context '16 - 01-03- Ni**as In Paris' do
+      #   let(:file) { '01-03- Ni**as In Paris.mp3' }
+      #   let(:fingerprint) { "#{folder}01-03- Ni**as In Paris.txt" }
+
+      #   it 'returns the correct result' do
+      #     aggregate_failures do
+      #       expect(identify['results'].count).to be(2)
+      #       expect(identify['status']).to eq('ok')
+      #       expect(identify.dig('results', 0, 'id')).to eq('wrong')
+      #       expect(identify.dig('results', 1, 'id')).to eq('wrong')
+      #     end
+      #   end
+      # end
     end
   end
 end

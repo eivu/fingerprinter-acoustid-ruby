8651be881be81e4fbb3a9973a132c349fb424c1b
Robert Jenkins <bobert@gmail.com>
2021-06-18 17:38:37 -0400

    refactored base class

--- lib/eivu/objects/base_class.rb
+++ lib/eivu/objects/base_class.rb
@@ -3,31 +3,25 @@
 module Eivu
   module Objects
     class BaseClass
-      # alias eql?  ==
-      # alias equal ==
 
       def ==(other)
         other.class == self.class && other&.id == id
       end
 
-      # def eql?(other)
-      #   self == other
-      # end
-
       def hash
         state.hash
+      end
+
+      def state
+        instance_variables.map { |variable| instance_variable_get variable }
       end
 
       # def ==(other)
       #   other.class == self.class && other.state == self.state
       # end
 
-      # def state
-      #   self.instance_variables.map { |variable| self.instance_variable_get variable }
-      # end
-
-      alias_method :eql?,  :==
-      alias_method :equal?, :==
+      alias eql?   ==
+      alias equal? ==
     end
   end
 end

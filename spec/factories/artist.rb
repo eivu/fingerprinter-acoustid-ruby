477f53a01e9cf468730332d375d6b7f7dd7c11df
Robert Jenkins <bobert@gmail.com>
2021-06-27 11:59:41 -0400

    added support for factories to app

---
+++ spec/factories/artist.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :artist, class: Eivu::Objects::Artist do
+    id { Faker::Crypto.sha1 }
+    name { Faker::Music.band }
+    joinphrase { nil }
+
+    trait :with_joinphrase do
+      joinphrase { ' & '}
+    end
+
+    initialize_with { new(id: id, name: name, joinphrase: joinphrase) }
+  end
+end
